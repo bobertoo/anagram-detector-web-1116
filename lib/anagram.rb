c1b5890f290e0a051a1d969ad8baa2da648c3492
Vinny Martinez <vinny@vinnymartinez.com>
2016-11-30 18:42:10 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,10 @@
+require 'pry'
 class Anagram
   def initialize(word)
     @word = word
+  end
+
+  def match(array)
+    array.select {|w| w.split('').sort.join == @word.split('').sort.join}
   end
 end
